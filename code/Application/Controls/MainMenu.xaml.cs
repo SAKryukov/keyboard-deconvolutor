3459b84def093e4228f3d41d961f65892b4ffed7
Sergey A Kryukov <SAKryukov@users.noreply.github.com>
2026-07-14 16:46:27 -0400

    Code: new menu style, no flawed Microsoft template

---
+++ code/Application/Controls/MainMenu.xaml.cs
@@ -0,0 +1,14 @@
+namespace SA.Controls;
+using ScanCode = Semantic.ScanCode;
+using Canvas = System.Windows.Controls.Canvas;
+using Rectangle = System.Windows.Shapes.Rectangle;
+using TextBlock = System.Windows.Controls.TextBlock;
+using Brush = System.Windows.Media.Brush;
+
+public partial class MainMenu : System.Windows.Controls.ContextMenu {
+
+    public MainMenu() {
+        InitializeComponent();
+    } //MainMenu
+
+} //class MainMenu
